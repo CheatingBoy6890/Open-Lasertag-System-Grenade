c93c43833733d6e225fd4f391c4e8ea7633bd3bc
CyberFox3801 <hille.schafe@gmail.com>
2024-08-21 14:07:24 +0200

    Added the batter cover stl

--- src/main.cpp
+++ src/main.cpp
@@ -5,104 +5,117 @@
 #include <IRsend.h>
 
 #define NUMLEDS 8
-#define LEDPIN 2
-#define RECEIVERPIN 1
-#define Button 3
-#define IRPIN 4
+#define LEDPIN D2
+#define RECEIVERPIN D1
+#define Button D3
+#define IRPIN D4
 #define dammage 15
-int teamColor [4]{
-0xff0000,
-0x0000ff,
-0xffff00,
-0x00ff00
-
+#define sound D5 //sound will be implemented soon
+uint32_t teamColor[4]{
+    0xff0000,
+    0x0000ff,
+    0xffff00,
+    0x00ff00
 
 };
 
-uint8_t team;
-uint8_t playerID;
+uint8_t team ;
+uint8_t playerID ;
 decode_results results;
 
-bool volatile  goBooom;
+bool volatile goBooom = 0;
+bool isConnected =0;
 
-Adafruit_NeoPixel pixels(NUMLEDS,LEDPIN,NEO_GRB +NEO_KHZ800);
+float voltage;
+Adafruit_NeoPixel pixels(NUMLEDS, LEDPIN, NEO_GRB + NEO_KHZ800);
 IRrecv receiver(RECEIVERPIN);
 IRsend transmitter(IRPIN);
 
- void  IRAM_ATTR   buttonInterrupt();
- void booom ();
+//void IRAM_ATTR buttonInterrupt();
+void booom();
 
 
- 
 
-// put function declarations here:
-int myFunction(int, int);
-
-
-void setup() {
-pixels.begin();
-pixels.fill(0xffffff);
-pixels.show();
-receiver.enableIRIn();
-pinMode(Button,INPUT_PULLUP);
-attachInterrupt(digitalPinToInterrupt(Button),buttonInterrupt,FALLING);
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Seial has bagun");
+  pixels.begin();
+  pixels.fill(0xffffff);
+  pixels.show();
+  receiver.enableIRIn();
+  //attachInterrupt(digitalPinToInterrupt(Button), buttonInterrupt, FALLING);
+  pinMode(Button,INPUT_PULLUP);
+  pinMode(A0,INPUT);
+  transmitter.begin();
 }
 
-void loop() {
-  delay(1);
- while (team==NULL||playerID==NULL)
- {
-  if(receiver.decode(&results)){
-if(results.decode_type==MILESTAG2){
-team=results.command >> 4;
-playerID=results.address;
-pixels.setBrightness(50);
-pixels.fill(teamColor[team]);
-pixels.show();
+void loop()
+{
+   voltage = analogRead(A0) * 0.005394;
+  
+  delay(100);
+  Serial.println(digitalRead(Button));
+  while (!isConnected)
+  {
+    Serial.println("Connection loop");
+    if (receiver.decode(&results))
+    {
 
-}
+      if (results.decode_type == MILESTAG2)
+      {
+        team = results.command >> 4;
+        playerID = results.address;
+        pixels.setBrightness(50);
+        pixels.fill(teamColor[team]);
+        pixels.show();
+        Serial.print("set id and team to");
+        Serial.print("Team:");
+        Serial.println(team);
+       Serial.printf("PlayerID:%i\n",playerID);
+       isConnected=true;
+      }
+      receiver.resume();
+    }
+    delay(100);
   }
- }
-
-
-
-
-
+  booom();
 }
 
-void IRAM_ATTR buttonInterrupt(){
-goBooom=1;
+/*void IRAM_ATTR buttonInterrupt()
+{
+  goBooom = 1;
+} */
+void booom()
+{
+  if (digitalRead(Button)==0)
+  {
+    pixels.setBrightness(255);
+    for (int i = 200; i > 0; i-=10)
+    {
+      pixels.fill(teamColor[team]);
+      pixels.show();
+      delay(i);
+      pixels.fill();
+      pixels.show();
+      delay(i);
+    }
+    pixels.fill(teamColor[team]);
+    pixels.show();
+    uint32_t data = 0;
+    data = playerID;
+    data = data << 2;
+    data = data + team;
+    data = data << 4;
+    data = data + dammage;
+    // transmitter.sendMilestag2(data);
+    transmitter.sendMilestag2(data, 14, 0);
 
-}
-void booom(){
-if(goBooom){
-  pixels.setBrightness(255);
-for(int i =200;i>0; i--){
-pixels.fill(teamColor[team]);
+    goBooom = 0;
+    pixels.fill();
+    pixels.show();
+  }else if(voltage<=2.8){
+pixels.fill(0xff00ff);
 pixels.show();
-delay(i);
-pixels.fill();
-pixels.show();
-delay(i);
-
-
-
-}
-pixels.fill(teamColor[team]);
-pixels.show();
-  uint32_t data = 0;
-  data = playerID;
-  data = data << 2;
-  data = data + team;
-  data = data << 4;
-  data = data + dammage;
-  // transmitter.sendMilestag2(data);
-  transmitter.sendMilestag2(data, 14, 0);
-
-goBooom=0;
-pixels.fill();
-pixels.show();
-
-
-}
+  }
 }
